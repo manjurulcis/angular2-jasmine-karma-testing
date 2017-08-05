0f7a089974e5b8853177e5d25604e9777a82258e
Md Manzurul Haque <haque.mdmanzurul@gmail.com>
2017-08-05 19:17:48 +0300

    Updated the unit test file

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,8 @@
-/* tslint:disable:no-unused-variable */
-
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+
+import { MyService } from './my-service'
+import { MyFakeService } from './my-fake-service'
 
 describe('AppComponent', () => {
   beforeEach(() => {
@@ -9,26 +10,34 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      providers:[{provide:MyService,useClass:MyFakeService}] // --> new code
     });
     TestBed.compileComponents();
   });
 
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    let fixture = TestBed.createComponent(AppComponent);
+    let app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
 
   it(`should have as title 'app works!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    let fixture = TestBed.createComponent(AppComponent);
+    let app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app works!');
   }));
 
   it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+    let fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
+    let compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('app works!');
   }));
+
+  it('should attach message from service to component', async(() => {
+    let fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    expect(fixture.componentInstance.message).toBe('fake service');
+  }));
+
 });
